9d52d95874326a4cfdf9257c23c40587f72ff253
tanmaivan <tan.maivande@gmail.com>
2025-10-11 14:08:33 +0700

    feat: update dbt_flow to support Prefect integration

--- orchestration/dbt_flow.py
+++ orchestration/dbt_flow.py
@@ -1,12 +1,49 @@
+import os
+import yaml
 from prefect import flow
-from prefect_shell import ShellOperation
+from prefect_gcp import GcpCredentials
+from prefect_dbt import PrefectDbtRunner, PrefectDbtSettings
+from pathlib import Path
 
 
 @flow
 def run_dbt():
-    ShellOperation(
-        commands=["dbt deps", "dbt run", "dbt test"], working_dir="dbt_hacker_news"
-    ).run()
+    gcp = GcpCredentials.load("gcp-creds")
+    profiles_dir = "/tmp/dbt_profiles"
+    os.makedirs(profiles_dir, exist_ok=True)
+
+    profile = {
+        "dbt_hacker_news": {
+            "target": "dev",
+            "outputs": {
+                "dev": {
+                    "type": "bigquery",
+                    "method": "service-account-json",
+                    "project": gcp.project,
+                    "keyfile_json": gcp.service_account_info.get_secret_value(),  # sửa chỗ này
+                    "dataset": "hn_dev",
+                    "location": "asia-southeast1",
+                    "threads": 4,
+                }
+            },
+        }
+    }
+
+    with open(f"{profiles_dir}/profiles.yml", "w") as f:
+        yaml.dump(profile, f)
+
+    project_path = str(Path(__file__).resolve().parents[1] / "dbt_hacker_news")
+
+    settings = PrefectDbtSettings(
+        project_dir=project_path,
+        profiles_dir=profiles_dir,
+    )
+
+    runner = PrefectDbtRunner(settings=settings)
+
+    runner.invoke(["deps"])
+    runner.invoke(["run"])
+    runner.invoke(["test"])
 
 
 if __name__ == "__main__":
